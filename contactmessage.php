c958242c2cb8628868836104d14720d47b67391a
rmanales10 <rmanales10@gmail.com>
2024-06-03 23:10:38 +0800

    dsadasd

--- contactmessage.php
+++ contactmessage.php
@@ -7,7 +7,7 @@ $dbname = "compassionmemorialgardens";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
+// Check connectiond
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
